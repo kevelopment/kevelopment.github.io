1e519efa75656be34f2dff70492e1d00cde3f589
adamkev418 <kevin.adam@lhind.dlh.de>
2020-10-10 14:02:19 +0200

    initial commit

---
+++ precache-manifest.61fc624121f3121ac682f8871adfe81e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ebdba50f4344fceb860a8e727dacda9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bd6ee0c069eb1bedca3",
+    "url": "/static/css/main.f72a8f5b.chunk.css"
+  },
+  {
+    "revision": "cb5e786d11cd48343c0e",
+    "url": "/static/js/2.d80a1e8d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.d80a1e8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bd6ee0c069eb1bedca3",
+    "url": "/static/js/main.afb76dfb.chunk.js"
+  },
+  {
+    "revision": "fe6861f28031af2fb99f",
+    "url": "/static/js/runtime-main.7f116512.js"
+  }
+]);
