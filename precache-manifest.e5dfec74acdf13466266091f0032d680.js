36b12621f65aba4a50b004e686a28aba4f787f19
Kevin Adam <kevinadam@gmx.de>
2020-10-17 14:24:58 +0200

    added some kind of files

---
+++ precache-manifest.e5dfec74acdf13466266091f0032d680.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f31d2bf50b5082514574a90447968b57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "448f99c7c2cfb8f5c80e",
+    "url": "/static/css/main.aa2f3cef.chunk.css"
+  },
+  {
+    "revision": "fdd9932dae4477e9880a",
+    "url": "/static/js/2.da9322a4.chunk.js"
+  },
+  {
+    "revision": "556d72da1c19c1e88e2917fa6505d7f0",
+    "url": "/static/js/2.da9322a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "448f99c7c2cfb8f5c80e",
+    "url": "/static/js/main.cb38f9bf.chunk.js"
+  },
+  {
+    "revision": "fe6861f28031af2fb99f",
+    "url": "/static/js/runtime-main.7f116512.js"
+  }
+]);
