e8b0425afe416490da524ad045c65c1a2cf689fd
vudor <kevinadam@gmx.de>
2020-10-17 12:57:33 +0000

    Update from https://github.com/vudor/doggo-support/commit/307b203c4e1b9fd294a5a45c8c5cd6d6a2bf5e13

--- precache-manifest.8bd688bf55b97bf3e119a900ea9f2959.js
+++ precache-manifest.8bd688bf55b97bf3e119a900ea9f2959.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f31d2bf50b5082514574a90447968b57",
+    "revision": "71c8f6bb7c3bea09971299d96f055c9d",
     "url": "/index.html"
   },
   {
-    "revision": "448f99c7c2cfb8f5c80e",
+    "revision": "8a3b2b1ef183fa2fa6da",
     "url": "/static/css/main.aa2f3cef.chunk.css"
   },
   {
-    "revision": "fdd9932dae4477e9880a",
-    "url": "/static/js/2.da9322a4.chunk.js"
+    "revision": "82c39d31b8e59b1af114",
+    "url": "/static/js/2.04947b8d.chunk.js"
   },
   {
     "revision": "556d72da1c19c1e88e2917fa6505d7f0",
-    "url": "/static/js/2.da9322a4.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.04947b8d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "448f99c7c2cfb8f5c80e",
-    "url": "/static/js/main.cb38f9bf.chunk.js"
+    "revision": "8a3b2b1ef183fa2fa6da",
+    "url": "/static/js/main.7eedf5ce.chunk.js"
   },
   {
     "revision": "fe6861f28031af2fb99f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e5dfec74acdf13466266091f0032d680.js"
+  "/precache-manifest.8bd688bf55b97bf3e119a900ea9f2959.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
