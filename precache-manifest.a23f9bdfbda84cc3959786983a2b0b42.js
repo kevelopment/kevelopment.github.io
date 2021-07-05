07d0720e0b40a0a8191cab0ec236ad7c252786cf
vudor <kevinadam@gmx.de>
2021-07-05 07:29:57 +0000

    Update from https://github.com/kevelopment/doggo-license-trainer/commit/5b74e84de817c3af9817c733d7c005a33b5a2550

---
+++ precache-manifest.a23f9bdfbda84cc3959786983a2b0b42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3975479718e7dd735b1e35c324c2add",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4bd7c2731bbd56921f6",
+    "url": "/static/css/main.aa2f3cef.chunk.css"
+  },
+  {
+    "revision": "82c39d31b8e59b1af114",
+    "url": "/static/js/2.04947b8d.chunk.js"
+  },
+  {
+    "revision": "556d72da1c19c1e88e2917fa6505d7f0",
+    "url": "/static/js/2.04947b8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4bd7c2731bbd56921f6",
+    "url": "/static/js/main.e7fa188f.chunk.js"
+  },
+  {
+    "revision": "fe6861f28031af2fb99f",
+    "url": "/static/js/runtime-main.7f116512.js"
+  }
+]);
